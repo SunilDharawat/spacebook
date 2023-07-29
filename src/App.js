bd1ccab418eed8c08eac73d47262311379b34f3e
SunilDharawat <sunildharawal2001@gmail.com>
2023-07-29 19:04:44 +0530

    App.js update commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from "react";
-
-import Signup from "./componets/signup/Signup";
 import { Route, Routes, } from "react-router-dom";
 import Home from "./routes/Home";
 
@@ -9,7 +7,6 @@ function App() {
   return (
     <div>
         <Routes>
-        <Route path="/signup" element={<Signup />} />
         <Route path="/home" element={<Home/>} />
         </Routes>
    </div>
